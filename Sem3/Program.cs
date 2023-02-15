9e42422a61e22c866bb4b3ef80ec80f9e26828d6
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-15 22:00:12 +0300

    add Задача 3 будут дополнения деления на ноль

---
+++ Sem3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Программа, которая на вход будет принимать 2 числа и выводить, является ли второе число кратным первому, а если нет, то выводить остаток!
+
+Console.WriteLine("Введите первое число: ");
+int.TryParse(Console.ReadLine(), out int first);
+Console.WriteLine("Введите первое число: ");
+int.TryParse(Console.ReadLine(), out int second);
+
+if (first > second)
+{
+  int compare = first % second;
+  if (compare == 0) Console.WriteLine($"Число {first} кратно {second}");
+  else Console.WriteLine($"Остаток от деления = {compare}");
+}
+// else if (first == 0) ///////////////////// подробности деления на нольбудут укзаны далее!!!!!!
+// {
+//   int compare = first % second;
+//   if (compare == 0) Console.WriteLine($"Число {first} кратно {second}");
+//   else Console.WriteLine($"Остаток от деления = {compare}");
+// }
+// else if (second == 0)
+// {
+//   int compare = second  % first;
+//   if (compare == 0) Console.WriteLine($"Число {second } кратно {first}");
+//   else Console.WriteLine($"Остаток от деления = {compare}");
+// }
+else
+{
+  int compare = second  % first;
+  if (compare == 0) Console.WriteLine($"Число {second } кратно {first}");
+  else Console.WriteLine($"Остаток от деления = {compare}");
+}
