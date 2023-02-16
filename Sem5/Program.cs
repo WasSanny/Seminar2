7b67dc76d13572b1f98f908c74d2bd13bf24ade8
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-16 22:28:35 +0300

    add дополнения для ввода отрицательных чисел

--- Sem5/Program.cs
+++ Sem5/Program.cs
@@ -1,27 +1,27 @@
 ﻿// // Программа, которая принимает на вход два числа и проверяет,
 // // является ли одно из них квадратом другого.
 
-// Console.WriteLine("Введите первое число: ");
-// int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
 
-// Console.WriteLine("Введите второе число: ");
-// int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
 
-//   if (a == Math.Sqrt(b) || b == Math.Sqrt(a))
-//   {
-//     Console.WriteLine("Да!");
-//   }
-//   else
-//   {
-//     Console.WriteLine("Нет!"); 
-//   }
+  if (Math.Abs(a) == Math.Sqrt(b) || Math.Abs(b) == Math.Sqrt(a))
+  {
+    Console.WriteLine("Да!");
+  }
+  else
+  {
+    Console.WriteLine("Нет!"); 
+  }
 
 
 
 // Вывести в консоль квадрат из звездочек
 
-Console.WriteLine(@"* * * * *");
-Console.WriteLine(@"*       *");
-Console.WriteLine(@"*       *");
-Console.WriteLine(@"*       *");
-Console.WriteLine(@"* * * * *");
+// Console.WriteLine(@"* * * * *");
+// Console.WriteLine(@"*       *");
+// Console.WriteLine(@"*       *");
+// Console.WriteLine(@"*       *");
+// Console.WriteLine(@"* * * * *");
