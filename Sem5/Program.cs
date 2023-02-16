76d9d1d1d517d5db473dcf7e9d01fb91035694e7
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-16 22:21:33 +0300

    add Рисуем квадрат в консоли

--- Sem5/Program.cs
+++ Sem5/Program.cs
@@ -1,17 +1,27 @@
-﻿// Программа, которая принимает на вход два числа и проверяет,
-// является ли одно из них квадратом другого.
+﻿// // Программа, которая принимает на вход два числа и проверяет,
+// // является ли одно из них квадратом другого.
 
-Console.WriteLine("Введите первое число: ");
-int a = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите первое число: ");
+// int a = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Введите второе число: ");
-int b = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b = int.Parse(Console.ReadLine());
 
-  if (a == Math.Sqrt(b) || b == Math.Sqrt(a))
-  {
-    Console.WriteLine("Да!");
-  }
-  else
-  {
-    Console.WriteLine("Нет!"); 
-  }
+//   if (a == Math.Sqrt(b) || b == Math.Sqrt(a))
+//   {
+//     Console.WriteLine("Да!");
+//   }
+//   else
+//   {
+//     Console.WriteLine("Нет!"); 
+//   }
+
+
+
+// Вывести в консоль квадрат из звездочек
+
+Console.WriteLine(@"* * * * *");
+Console.WriteLine(@"*       *");
+Console.WriteLine(@"*       *");
+Console.WriteLine(@"*       *");
+Console.WriteLine(@"* * * * *");
